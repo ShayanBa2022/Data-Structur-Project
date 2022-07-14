1be120bfaa88c6d565ed25b22da10f4975b5ac58
Shayan <shayanbd2001@gmail.com>
2022-07-14 21:15:06 +0430

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Main {
@@ -11,39 +12,52 @@ public class Main {
     static int[] dist = new int[MAX];
     static int[] parent = new int[MAX];
     static boolean[] found = new boolean[MAX];
+    static int numberOfVertices, numberOfEdges;
     //------------------------------------------------------------------------------------------------------------------
-    class Pair{
-        static int first;
-        static int second;
-    }
-    //------------------------------------------------------------------------------------------------------------------
-    class d_aryHeap{
+    static class d_aryHeap {
         private int d; // number of children each node can have.
         private int heapSize; // equals to the number of nodes we insert
         private int[] mainHeap; // Heap using array.
+        private int[] distance; // dstance from each node to next node
+
         // -------------------------------------------------------------------------------------------------------------
         // Building constructor
-        public d_aryHeap(int numberOfChildren , int numberOfVertices ){
-            d = numberOfChildren;
+        public d_aryHeap(int numberOfChildren, int numberOfVertices) {
+            d = numberOfVertices/numberOfEdges;
             mainHeap = new int[numberOfVertices]; // Building a heap with 'MAX' size.
             Arrays.fill(mainHeap, INF); // All elements in heap are now infinity
+            Arrays.fill(distance, INF); // all distances sets to INF
         }
+
         //--------------------------------------------------------------------------------------------------------------
         // Function to find index 'i' parent
-        private int parent(int i){
-            return (i-1) / d ; // Formula based on the d_ary heap definition.
+        private int parent(int i) {
+            return (i - 1) / d; // Formula based on the d_ary heap definition.
         }
+
         //--------------------------------------------------------------------------------------------------------------
         // Function to find k'th children of node 'i'
-        private int child(int i , int k){
-            return ( d * i ) + k ; // Based on d_ary heap definition.
+        private int kthChild(int i, int k) {
+            return (d * i) + k; // Based on d_ary heap definition.
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+        private int minChild(int index){
+            int bestChild = kthChild((index,1));
+            int k = 2; // setting child to 2
+            int KthChildofindex = kthChild((index,k));
+            while((k <= d) && KthChildofindex < heapSize){
+                if (mainHeap[KthChildofindex] < mainHeap[bestChild])
+                    bestChild = KthChildofindex;
+                KthChildofindex = kthChild(index,k++)
+            }
         }
 
         //--------------------------------------------------------------------------------------------------------------
         // Upward heapify
-        private void upwardHeapify(int childIndex){
+        private void upwardHeapify(int childIndex) {
             int temp = mainHeap[childIndex]; // saving the value saved in the node in the heap
-            while ( childIndex > 0 && temp < mainHeap[parent(childIndex)]){
+            while (childIndex > 0 && temp < mainHeap[parent(childIndex)]) {
                 // changing parent and child with each other
                 mainHeap[childIndex] = mainHeap[parent(childIndex)];
                 childIndex = parent(childIndex);
@@ -53,46 +67,64 @@ public class Main {
 
         //--------------------------------------------------------------------------------------------------------------
         // Downward heapify
-        private void downwardHeapify(int index){ ////// need to be done
+        private void downwardHeapify(int index) { ////// need to be done
             int child;
             int temp = mainHeap[index];
-            while(index < heapSize){
-                //child =
+            while(kthChild(index,1) < heapSize){
+                child = minChild(index);
+                if (mainHeap[child] < temp)
+                    mainHeap[index] = mainHeap[child];
+                else
+                    break
+                index = child;
             }
+            mainHeap[index] = temp;
         }
 
         //--------------------------------------------------------------------------------------------------------------
         //Function to add elements to heap
-        public void insertToHeap(int x){
+        public void insertToHeap(int x) {
             int heapIndex = 0;
-            while (heapIndex != MAX){
+            while (heapIndex != MAX) {
                 if (mainHeap[heapIndex] == INF) {
                     mainHeap[heapIndex] = x;
                     upwardHeapify(numberOfVertices - 1);
                     break;
-                }
-                else
-                    heapIndex++ ;
+                } else
+                    heapIndex++;
             }
         }
 
         //---------------------------------------------------------------------------------------------------------------
         // Function to delete element from heap
-        public int deleteFromHeap(int index){
+        public int deleteFromHeap(int index) {
             int temp = mainHeap[index];
             mainHeap[index] = mainHeap[heapSize - 1];
-            heapSize-- ;
+            heapSize--;
             downwardHeapify(index);
             return temp;
         }
+
+
+        //------------------------------------------------------------------------------------------------------------------
+        public int findMinimum() {
+            if(heapSize == 0){
+                throw new NoSuchElementException("Empty heap");
+            }
+            return mainHeap[0];
+        }
     }
+
 
     //------------------------------------------------------------------------------------------------------------------
     //vector<pair<int,int> > adj[MAX]; needs to be translated to java
+    ArrayList<Map<int,int>> adj[MAX];
     //static ArrayList<Pair> [] adj = new ArrayList<>;
     static int numberOfVertices, numberOfEdges;
 
     public static int main(String[] args) {
+        d = numberOfEdges / numberOfVertices;
+
         /////////////////////////////////////////
         /////////////////
         //Sample Input (CLRS-Page 659):
@@ -112,20 +144,18 @@ public class Main {
         int source;
         Scanner scanner = new Scanner(System.in);
         numberOfVertices = scanner.nextInt();
-        scanner.next();
+        //scanner.next();
         numberOfEdges = scanner.nextInt();
-        scanner.next();
+        //scanner.next();
         source = scanner.nextInt();
         // cin >> numberOfVertices >> numberOfEdges >> source;
-        int dimension = numberOfEdges / numberOfVertices;
-
+        //int dimension = numberOfEdges / numberOfVertices;
+        scanner.nextLine();
         // edge x to y with weight of 'weight'
         int x, y, weight;
         for (int i = 0; i < numberOfEdges; i++) {
             x = scanner.nextInt();
-            scanner.next();
             y = scanner.nextInt();
-            scanner.next();
             weight = scanner.nextInt();
             //cin >> x >> y >> weight; // edge x->y with length weight
 
@@ -158,25 +188,9 @@ public class Main {
         //------------------------------------------
         dist[s] = 0;
         for (int i = 0; i < numberOfVertices; i++) {
-
-            /////////////////////////////////////////////////////
-            //Replace this part with
-            //	int v = Extract Minimum from a (d-ary)-Heap (CLRS-Page 167)
-            //and change the required parts of the program accordingly.
-            //Notes: (1) Implement (d-ary)-Heap with array
-            //		 (2) Keep the childs of each node in the (d-ary)-Heap in regular Heap
-            //		 (3) d = [numberOfEdges / numberOfVertices]
-            /////////////////////////////////////////////////////
-            int v = -1;
-            for (int j = 0; j < numberOfVertices; j++) {
-                if (!found[j] && (v == -1 || dist[j] < dist[v]))
-                    v = j;
-            }
-            if (dist[v] == INF)
-                break;
+            v = dAryHeap.findMinimum();
             found[v] = true;
-
-            for (Object edge : adj[v]) {
+            for (int edge : adj[v]) {
                 int to = edge[0];
                 int len = edge.[1];
 
